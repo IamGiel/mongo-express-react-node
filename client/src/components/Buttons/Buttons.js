4c029e5787eb71ca055054915a04429acde7ff96
esharri2 <esharri2@gmail.com>
2018-06-06 19:08:18 -0400

    disable submit buttons with no input

--- client/src/components/Buttons/Buttons.js
+++ client/src/components/Buttons/Buttons.js
@@ -3,7 +3,7 @@ import React from "react";
 // Destructuring the type, className, children and onClick props, applying them to the button element
 const Button = ({ type = "default", className, disabled, children, onClick }) => (
   <button
-  disabled={disabled}
+    disabled={disabled}
     onClick={onClick}
     className={["btn", `btn-${type}`, className].join(" ")}
   >
